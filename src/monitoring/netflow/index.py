3900fef44a241b3e1287a654554e3ed815a6d760
Samiuela LV Taufa <samtaufa@gmail.com>
2011-03-10 16:23:03 +1100

    netflow documentation update

---
+++ src/monitoring/netflow/index.py
@@ -0,0 +1,31 @@
+from countershape.doc import *
+import countershape
+from countershape import  markup
+
+this.markup = markup.Markdown()
+this.layout = ns.tpl_layout
+
+this.titlePrefix = ns.titlePrefix + "[Netflow] "
+
+pages = [
+               
+    Page("sensor.mdtext", 
+        title="Sensor",
+        pageTitle="Sensor"),    
+
+    Page("collector.mdtext", 
+        title="Collector",
+        pageTitle="Collector"),    
+
+	Page("flow.views.mdtext",
+		title="Flow - Console",
+		pageTitle="netflow Views"),
+
+	Page("flow.flowscan.mdtext",
+		title="Flow - Graphs",
+		pageTitle="netflow Graphs - Flowscan and CUFlow"),
+
+	Page("flow.flowviewer.mdtext",
+		title="Flow - Custom",
+		pageTitle="netflow Graphs - FlowViewer"),
+]
