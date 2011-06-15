8b6d0aa9bf835174aea0fd84432a7f21fea44e35
Samiuela LV Taufa <samtaufa@gmail.com>
2011-06-15 10:11:12 +1000

    markup.Markdown(extras=["code-friendly"]) use syntax("file-type") highlighting

--- src/monitoring/index.py
+++ src/monitoring/index.py
@@ -1,8 +1,9 @@
 from countershape.doc import *
 import countershape
-from countershape import  markup
 
-this.markup = markup.Markdown()
+#from countershape import  markup
+#this.markup = markup.Markdown(extras=["code-friendly"])
+
 this.layout = ns.tpl_layout
 
 ns.blk_sidemenu = countershape.widgets.SiblingPageIndex(
